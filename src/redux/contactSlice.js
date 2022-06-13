4ce03fd0b7a501076aaffb82d369188090df79cc
Hami Berkay Aktaş <hberkayaktas@gmail.com>
2022-06-13 15:39:27 +0300

    redux and slice eklendi
    sonraki işlem data normalleştirme

---
+++ src/redux/contactSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+const contactSlice = createSlice({
+  name: "contacts",
+  initialState: {
+      items:[]
+  },
+  reducers: {},
+});
+export default contactSlice.reducer;
